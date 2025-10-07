816b798bd01b16e7d50473e90877c9b2ce0f683f
rdufourUCI <rdufour@uci.edu>
2025-10-06 18:57:21 -0700

    tttt
    tttt

--- In Class Activities/Assets/W2/Scripts/Ball.cs
+++ In Class Activities/Assets/W2/Scripts/Ball.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -11,104 +11,73 @@ public class Ball : MonoBehaviour
 
     private int _bounces;
 
-    // ------------------------------------------------------------------------
-    // This code runs whenever the ball hits something.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // STEP 1 -------------------------------------------------------------
-        // Uncomment ONE of the blow lines of code to ADD ONE
-        //      to the value of the variable named '_bounces'.
-        //_bounces = _bounces + 2;
-        //_bounces + 1
-        //_bounces --;
-        //_bounces++;
-        //_bounces += 1
+        // Add one to the value of '_bounces' and update text
+        _bounces += 1;
+        _bouncesText.text = "Bounces: " + _bounces;
         // STEP 1 -------------------------------------------------------------
 
-        // This line sets the value of the text on screen to the word "Bounces"
-        //      plus the number of times the ball has bounced.
-        // We can use the addition operator on strings too!
-        // It concatenates (combines) the strings together into one long string.
-        _bouncesText.text = "Bounces: " + _bounces;
-
-        // In data, colors are usually stored as "RGB channels", meaning,
-        //      Red, Green, and Blue are mixed together in different intensities
-        //      to create the color.
-        // These variables represent the Red, Green, and Blue values
-        //      of the color of your ball.
+        // Get current RGB values
         float r = _spriteRenderer.color.r;
         float g = _spriteRenderer.color.g;
         float b = _spriteRenderer.color.b;
 
         // STEP 2 -------------------------------------------------------------
-        // Uncomment ONE of the below lines to ADD 0.1 to the value of 'r'.
-        //r += 0.1f
-        //r * 0.1;
-        //r = r + 0.1f;
-        //r = r * 0.1f;
-        //r += 0.1;
+        // Add 0.1 to the red channel
+        r += 0.1f;
         // STEP 2 -------------------------------------------------------------
 
-        // RGB values can only go from 0.0 - 1.0.
-        // So, if the value of 'r' gets to above 1.0, we want to set it back to 0.0
-        //      so that we can start over and keep increasing it again.
+        // STEP 3 -------------------------------------------------------------
+        // If red goes above 1.0, reset to 0.0
         if (r > 1.0f)
         {
-            // STEP 3 ---------------------------------------------------------
-            // Uncomment ONE of the below lines of code to set the value of 'r' to ZERO.
-            //r = 0.0f
-            //r = 0.0f;
-            //r 0.0f;
-            // STEP 3 ---------------------------------------------------------
+            r = 0.0f;
         }
+        // STEP 3 -------------------------------------------------------------
 
         // STEP 4 -------------------------------------------------------------
-        // Uncomment the below line and fix it to SUBTRACT 0.1 from the value of 'g'.
-        //g -= 0.1f
+        // Subtract 0.1 from green each bounce
+        g -= 0.1f;
         // STEP 4 -------------------------------------------------------------
 
+        // STEP 5 -------------------------------------------------------------
+        // If green goes below 0.0, reset to 1.0
         if (g < 0.0f)
         {
-            // STEP 5 ---------------------------------------------------------
-            // Uncomment the below line and fix it to set the value of 'g' to ONE.
-            //g = 1.0;
-            // STEP 5 ---------------------------------------------------------
+            g = 1.0f;
         }
+        // STEP 5 -------------------------------------------------------------
 
         // STEP 6 -------------------------------------------------------------
-        // Below this comment, write a line of code to MULTIPLY 'b' by 1.2.
-
+        // Multiply blue by 1.2 to increase intensity
+        b *= 1.2f;
         // STEP 6 -------------------------------------------------------------
 
         // STEP 7 -------------------------------------------------------------
-        // Below this comment, write an IF STATEMENT:
-        // IF the value of 'b' is GREATER THAN **OR** EQUAL TO 1.0,
-        //      set 'b' to 0.1. 
-        
+        // If blue >= 1.0, reset it to 0.1
+        if (b >= 1.0f)
+        {
+            b = 0.1f;
+        }
         // STEP 7 -------------------------------------------------------------
 
-        // This line assigns a new color to your Ball with the new RGB values
-        //      we just created.
-        // Don't worry about how it works yet; we'll learn about this later :)
+        // Apply new color
         Color newColor = new Color(r, g, b);
         _spriteRenderer.color = newColor;
 
-        // This line prints the value of your new color to the Console window.
-        // We'll talk more about how this line works next week also :)
+        // Print RGB values to the Console
         Debug.Log(newColor);
 
         // STEP 8 -------------------------------------------------------------
-        // A simple way to calculate how bright a color will be is by
-        //      averaging the R, G, B values.
-        // Below this comment, write 2 lines of code to ADD the values of 
-        //      r, g, and b, and then divide the result by 3.
-        // Store the result in a variable named 'brightness'.
-        
+        // Calculate brightness as the average of R, G, and B
+        float brightness = (r + g + b) / 3f;
         // STEP 8 -------------------------------------------------------------
 
         // STEP 9 -------------------------------------------------------------
-        // Uncomment the below line to make the 'brightness' text change in your game.
-        //_brightnessText.text = "brightness = " + brightness;
+        // Display the brightness value on screen
+        _brightnessText.text = "Brightness = " + brightness.ToString("F2");
         // STEP 9 -------------------------------------------------------------
     }
 }
--- README.md
+++ README.md
@@ -4,9 +4,27 @@
 Hello World
 
 ### W2
-Create future Devlog sub-headers with the three # symbols, then write your Devlogs below them.
+
+Question 1 Response: The r, g, and b variables are floats because color values
+represented by decimal numbers between 0.0 and 1.0. Using floats allows for smooth and
+gradual color transitions, which wouldn't be possible with integers, booleans or strings.
+
+Question 2 Response: The _bounce variable is an integer because it keeps track of how
+many times the ball has bounced, which is always a whole number. A float would be unncessary, a boolan
+could only represent true or flase, and a string coulnd't be used for counting.
+
+Question 3 Response: The error that appeared after Step X of Part 2 showed 
+that there was a type of mismatch in the code. It revealed that the program
+was trying to use float values where a Color type was expected, which
+explained why the line didn't work. This helped show that the correct way to 
+set a color is by using "Color (r, g, b)" instead of adding the numbers together.
+
 
 ## Open-Source Assets
 ### W1
 - Animals: https://assetstore.unity.com/packages/3d/characters/animals/animals-free-animated-low-poly-3d-models-260727 
 - Low-poly environment: https://assetstore.unity.com/packages/3d/environments/landscapes/low-poly-simple-nature-pack-162153 
+
+
+
+
